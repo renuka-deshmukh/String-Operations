083a83324b2ab5534cba472fd617625e9a866c69
renuka-deshmukh <renuka.deshmukh776@gmail.com>
2025-06-10 22:36:10 +0530

    Updated

--- script.js
+++ script.js
@@ -1,39 +1,89 @@
 const inputString = document.querySelector('#inputPara')
 const result1 = document.querySelector("#Result")
 
-function countCharacters(){
+function countCharacters() {
 
-    inputSTR = inputString.value
-    console.log(inputSTR.length)
-    result1.textContent = inputSTR.length;
-    
+  inputSTR = inputString.value
+  console.log(inputSTR.length)
+  result1.textContent = inputSTR.length;
+
 }
 
-function countWords(){
+function palindrome() {
+  inputSTR = inputString.value
+
+  let reversed = "";
+  for (let i = inputSTR.length - 1; i >= 0; i--) {
+    reversed += inputSTR[i];
+  }
+  if (inputSTR === reversed) {
+    result1.textContent = "Palindrome";
+  } else {
+    result1.textContent = "Not Palindrome ";
+  }
+
+}
+
+function countWords() {
   inputSTR = inputString.value
 
   const wordCount = inputSTR.split(' ').length
-   
+
   result1.textContent = wordCount
 }
 
 
-function Vowels(){
-    const allVowels = 'aeiouAEIOU'
-    let count = 0;
-    inputSTR = inputString.value
+function Vowels() {
+  const allVowels = 'aeiouAEIOU'
+  let count = 0;
+  inputSTR = inputString.value
 
-    for(let i=0; i < inputSTR.length; i++){
-      if( allVowels.includes(inputSTR[i])){
-        count++;
-      }
-      result1.textContent = count
+  for (let i = 0; i < inputSTR.length; i++) {
+    if (allVowels.includes(inputSTR[i])) {
+      count++;
     }
+    result1.textContent = count
+  }
 
 }
 
-function ExtractFirstWord(){
-  inputSTR=inputString.value
+function ExtractFirstWord() {
+  inputSTR = inputString.value
 
   result1.textContent = inputSTR.charAt(0);
 }
+
+function ReplaceSpaces() {
+
+  inputSTR = inputString.value
+
+  const replaced = inputSTR.replaceAll(" ", "-")
+  result1.textContent = replaced
+}
+
+function SplitString() {
+  inputSTR = inputString.value
+
+  const str = inputSTR.split
+}
+
+function ReverseString() {
+
+  let reverse = "";
+  inputSTR = inputString.value
+
+  for (let i = inputSTR.length - 1; i >= 0; i--) {
+
+    reverse += inputSTR[i];
+  }
+  result1.textContent = reverse
+}
+
+
+
+
+
+
+
+
+
